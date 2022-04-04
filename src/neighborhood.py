7ececdb72a56ccc4879dbdb68fc3cda9ffeaa744
LucasVillenave <lucas.villenave33@gmail.com>
2022-04-04 16:25:19 +0200

    test

--- src/neighborhood.py
+++ src/neighborhood.py
@@ -10,6 +10,19 @@ Notion of neighborhoods:
     - You can do a circular permutation (Sweep)
 """
 
+def swapEvaluateur(solution,node1,node2):
+    graph = solution.getGraph()
+    partition = solution.getPartition()
+    nbClasses = solution.getNbClasses()
+    cost = 0
+    for c in range(nbClasses):
+        for c2 in range(nbClasses):
+            if c < c2:
+                for node in partition[c]:
+                    for node2 in partition[c2]:
+                        if graph.getEdge(node, node2) is not None:
+                            cost += 1
+    return cost
 
 def swap(solution):
     k = solution.getNbClasses()
