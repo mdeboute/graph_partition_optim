72a5e675751aadc0b14d56970bdc2b7de8e1a4fb
LucasVillenave <lucas.villenave33@gmail.com>
2022-04-16 11:12:26 +0200

    should be good

--- src/resultAutomatism.py
+++ src/resultAutomatism.py
@@ -3,6 +3,7 @@ from enumeration import basicEnum
 from utils import *
 from metaheuristics import *
 from main import initSol
+from gradient_descent import *
 
 # global basic params  
 k = 2
@@ -10,13 +11,20 @@ neighborhoodSize = 100
 iterMax = 1 # will be multiplyed by nbVertices
 timeOut = 6
 tabuSize = 7
+temperature = 36
+coolingRate = 0.09
+finalTemperature = 0.01
 
 doEnum = True
 
 doGradient = False
 doBetterGradient = False
 doTabou = False
-doTabouNSwap = True
+doTabouNSwap = False
+doRC = False
+doNSwapRC = False
+
+isInitSol = doGradient or doBetterGradient or doTabou or doTabouNSwap or doRC or doNSwapRC
 
 def resultAutomatism() :
     
@@ -36,9 +44,9 @@ def resultAutomatism() :
         "data/trenteSommets.txt",
         "data/cinquanteSommets.txt",
         "data/centSommets.txt",
-        #"data/cinqCentSommets.txt",
-        #"data/milleSommets.txt",
-        #"data/dixMilleSommets.txt",
+        "data/cinqCentSommets.txt",
+        "data/milleSommets.txt",
+        "data/dixMilleSommets.txt",
     ]
     for i in range (2,5) : 
         k = i
@@ -52,19 +60,43 @@ def do(path,k,neighborhoodSize,iterMax,timeOut,tabuSize) :
     t = time.time()
     graph = parse(path)
     
-    initialSolution, initialSolutionCost = initSol(graph,k)
+    if isInitSol :
+        initialSolution, initialSolutionCost = initSol(graph,k)
     
-    #Tabou
     if doTabou :
         t = time.time()
-        sol,solCost = tabuSearch( initialSolution, initialSolutionCost, iterMax*graph.getNbVertices(), timeOut, tabuSize, True, neighborhoodSize, True,)
+        sol,solCost = tabuSearch( initialSolution, initialSolutionCost, iterMax*graph.getNbVertices(), timeOut, tabuSize, False, neighborhoodSize, False,)
         print("\nTabou found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
+        
+    if doTabouNSwap :
+        t = time.time()
+        sol,solCost = tabuSearch( initialSolution, initialSolutionCost, iterMax*graph.getNbVertices(), timeOut, tabuSize, True, neighborhoodSize, True,)
+        print("\nTabou nSwap found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
     
-    #Enum
+    if doGradient :
+        t = time.time()
+        sol,solCost = gradientDescent( initialSolution, initialSolutionCost, timeOut, False, neighborhoodSize)
+        print("\nGD found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
+        
+    if doBetterGradient :
+        t = time.time()
+        sol,solCost = betterGradientDescent( initialSolution, initialSolutionCost, timeOut, False, neighborhoodSize)
+        print("\nbetterGD found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
+        
+    if doRC :
+        t = time.time()
+        sol,solCost = simulatedAnnealing( initialSolution, initialSolutionCost, swapNodes(initialSolution), iterMax*graph.getNbVertices(), timeOut, False, neighborhoodSize, temperature, finalTemperature, coolingRate)
+        print("\RC found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
+    
+    if doNSwapRC :
+        t = time.time()
+        sol,solCost = simulatedAnnealing( initialSolution, initialSolutionCost, swapNodes(initialSolution), iterMax*graph.getNbVertices(), timeOut, True, neighborhoodSize, temperature, finalTemperature, coolingRate)
+        print("\RC found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
+    
     if doEnum :
         t = time.time()
         sol,solCost = basicEnum( graph, timeOut, k)
-        print("\nTabou found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
+        print("\nEnum found the solution : ",sol," with a cost of ",solCost, " in ", time.time()-t,"seconds.")
 
 if __name__ == "__main__":
     resultAutomatism()
